e7c4f58bfec1ed1240cefd87b24ee63ebfadadf9
Ocean1029 <vanessa0201029@gmail.com>
2025-04-23 01:47:24 +0800

    調整鸚鵡塔的價格從 140 調整為 160，以增強遊戲平衡性。

--- monkey_shoot_balloon/towers/parrot.py
+++ monkey_shoot_balloon/towers/parrot.py
@@ -5,7 +5,7 @@ from projectiles.cannonball import Cannonball
 class Parrot(Tower):
     IMAGE = pygame.image.load("assets/images/tower/parrot.png")
     IMAGE = pygame.transform.scale(IMAGE, (40, 40))
-    PRICE = 140
+    PRICE = 160
 
     def __init__(self, x, y):
         super().__init__(x, y, range_radius=300, damage=0.5, attack_speed=4.0)
