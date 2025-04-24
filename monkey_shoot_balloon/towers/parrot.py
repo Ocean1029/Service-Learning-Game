f9430e8d823ae4649adf615aa24e35e50812adb0
Ocean1029 <vanessa0201029@gmail.com>
2025-04-24 20:54:25 +0800

    產生魔王關卡參數

--- monkey_shoot_balloon/towers/parrot.py
+++ monkey_shoot_balloon/towers/parrot.py
@@ -5,9 +5,9 @@ from projectiles.piercer import Piercer
 class Parrot(Tower):
     IMAGE = pygame.image.load("assets/images/tower/parrot.png")
     IMAGE = pygame.transform.scale(IMAGE, (40, 40))
-    PRICE = 160
+    PRICE = 170
 
     def __init__(self, x, y):
-        super().__init__(x, y, range_radius=340, damage=7.0, attack_speed=0.3)
+        super().__init__(x, y, range_radius=340, damage=6.5, attack_speed=0.3)
         self.rect = self.IMAGE.get_rect(center=(x, y))
         self.projectile_type = Piercer
