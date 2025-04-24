96b5d8815f970e5ff3b8e9f2c1984cabe5a639e5
Ocean1029 <vanessa0201029@gmail.com>
2025-04-24 20:54:25 +0800

    產生魔王關卡參數

--- monkey_shoot_balloon/enemies/tank_3.py
+++ monkey_shoot_balloon/enemies/tank_3.py
@@ -6,7 +6,7 @@ class Tank3(Enemy):
     IMAGE = pygame.image.load("assets/images/enemy/tank3.png")
     IMAGE = pygame.transform.scale(IMAGE, (40, 40))
     def __init__(self, path_points):
-        super().__init__(path_points=path_points, health=20, speed=60, reward=40)
+        super().__init__(path_points=path_points, health=20, speed=120, reward=20)
         # 先用圓形代替
         self.rect = self.IMAGE.get_rect()
         self.rect.center = (int(self.x), int(self.y))
--- monkey_shoot_balloon/enemies/tank_4.py
+++ monkey_shoot_balloon/enemies/tank_4.py
@@ -6,7 +6,7 @@ class Tank4(Enemy):
     IMAGE = pygame.image.load("assets/images/enemy/principal.png")
     IMAGE = pygame.transform.scale(IMAGE, (60, 60))
     def __init__(self, path_points):
-        super().__init__(path_points=path_points, health=100, speed=100, reward=500)
+        super().__init__(path_points=path_points, health=100, speed=200, reward=500)
         # 先用圓形代替
         self.rect = self.IMAGE.get_rect()
         self.rect.center = (int(self.x), int(self.y))
